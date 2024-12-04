624ef684031ce4c90af8b50cd31b171a5c76f8cc
Night <hotcoding85@gmail.com>
2024-12-04 21:10:10 +0300

    chore: fixed cors error for backend

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',  // Allow requests from your Next.js frontend
+    origin: 'http://172.16.3.88:3000',  // Allow requests from your Next.js frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow specific HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'],  // Allow specific headers
   });
